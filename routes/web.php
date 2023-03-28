04aa961339e228d512846145c892683516deca90
MarcoAgosta <agostamarco@libero.it>
2023-03-28 10:57:57 +0200

    Create rotte per il controller perfume

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\PerfumeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +29,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'verified'])
+    ->prefix("admin")
+    ->name("admin")
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name("dashboard");
+        Route::resource('/perfumes', PerfumeController::class);
+    });
 
 require __DIR__.'/auth.php';
