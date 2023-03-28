890e84bd9a5f260d98e397604e8d923e65ab4274
MarcoAgosta <agostamarco@libero.it>
2023-03-28 10:39:47 +0200

    Creato controller Perfume

--- app/Http/Controllers/PerfumeController.php
+++ app/Http/Controllers/PerfumeController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Perfume;
 use Illuminate\Http\Request;
 
 class PerfumeController extends Controller
 {
-    //
+    public function index(){
+        $perfumes=Perfume::all();
+        return view("perfumes.index", compact("perfumes"));
+    }
+
+    public function show($id){
+        $perfume=Perfume::findOrFail($id);
+        return view("perfumes.show", compact("perfume"));
+    }
+
+    public function create(){
+        return view("perfumes.create");
+    }
+
+    public function store(Request $request){
+        $data=$request->all();
+        $perfume= new Perfume();
+        $perfume->fill($data);
+        $perfume->save();
+
+        return redirect()->route("perfumes.show", $perfume->id);
+    }
+
+    public function edit($id){
+        $perfume=Perfume::find($id);
+        if(!$perfume){
+            abort(404, "Ritenta.");
+        };
+
+        return view("perfumes.edit", compact("perfume"));
+    }
+
+    public function update(Request $request, $id){
+        $data=$request->all();
+        $perfume=Perfume::findOrFail($id);
+        $perfume->fill($data);
+        $perfume->save();
+
+        return redirect()->route("perfumes.show", $perfume->id);
+    }
+
+    public function destroy($id){
+        $perfume=Perfume::findOrFail($id);
+        $perfume->delete();
+        return redirect()->route("perfumes.index");
+    }
 }
