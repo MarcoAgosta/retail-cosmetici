b9779fa8955dfdb301bb41dd0a4f16dcfbef3133
MarcoAgosta <agostamarco@libero.it>
2023-03-28 17:36:33 +0200

    Creata pagina show

--- resources/views/admin/perfumes/show.blade.php
+++ resources/views/admin/perfumes/show.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 <div class="container">
-    <h2>SHOW profumi</h2>
+    <div class="mt-5">
+        <h2>{{$perfume->name}}</h2>
+    </div>
+    <div class="row mt-4">
+        <div class="col-4 offset-1">
+            <img src="{{ asset('storage/' . $perfume->product_img) }}" class="w-100" alt="Immagine del profumo {{$perfume->name}}">
+        </div>
+        <div class="col-6 d-flex justify-content-between flex-column">
+            <p>
+                {{$perfume->description}}
+            </p>
+            <div class="d-flex justify-content-end">
+                <a href="{{route('admin.perfumes.edit', $perfume->id)}}" style="margin-right:1rem" class="btn btn-primary">Modifica profumo</a>
+                <a href="{{route('admin.perfumes.show', $perfume->id)}}"  class="btn btn-danger">Elimina profumo</a>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
