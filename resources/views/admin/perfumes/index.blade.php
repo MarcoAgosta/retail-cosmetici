304eae346393ce1d94b2e2ef11cc0370fc8f4a81
MarcoAgosta <agostamarco@libero.it>
2023-03-29 13:06:47 +0200

    Eliminato welcome e modificato index

--- resources/views/admin/perfumes/index.blade.php
+++ resources/views/admin/perfumes/index.blade.php
@@ -5,9 +5,11 @@
     <div class="row g-4 mt-4">
         @foreach($perfumes as $perfume)
         <div class="col-3">
-            <div class="card" >
-                <img class="card-img-top" src="{{ asset('storage/' . $perfume->product_img) }}" alt="Card image cap">
-                <div class="card-body">
+            <div class="card" style="height:425px" >
+                <div class="card-img-top">
+                    <img class="w-100" src="{{ asset('storage/' . $perfume->product_img) }}" alt="Card image cap">
+                </div>
+                <div class="card-body d-flex flex-column justify-content-end">
                     <h5 class="card-title mb-3 text-center">{{$perfume->name}}</h5>
                     <a href="{{route('admin.perfumes.show', $perfume->id)}}" class="btn btn-primary">Mostra il profumo</a>
                 </div>
@@ -16,4 +18,10 @@
         @endforeach
     </div>
 </div>
+
+<style>
+.card-img-top{
+    border-bottom: 1px solid lightgray;
+}
+</style>
 @endsection
