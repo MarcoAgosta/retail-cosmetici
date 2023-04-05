555510ae8be291e594ef47dab5d94f72eb10a5b6
MarcoAgosta <agostamarco@libero.it>
2023-04-05 17:24:14 +0200

    Creata ricerca

--- app/Http/Controllers/Api/PerfumeController.php
+++ app/Http/Controllers/Api/PerfumeController.php
@@ -8,12 +8,30 @@ use Illuminate\Http\Request;
 
 class PerfumeController extends Controller
 {
-    public function index(){
-        $perfumes=Perfume::paginate();
-        return response()->json([
-            'success'=>true,
-            'results'=>$perfumes
-        ]);
+    public function index(Request $request){
+
+        $search = $request->header('search');
+
+        if ($search===""){
+            $perfumes=Perfume::paginate();
+            return response()->json([
+                'success'=>true,
+                'results'=>$perfumes
+            ]);
+
+        }else{
+            // Search in the title and body columns from the posts table
+            $perfumes = Perfume::query()
+                ->where('name', 'like', '%' . $search . '%')
+                ->get();
+
+            // Return the search view with the resluts compacted
+            return response()->json([
+                'success'=>true,
+                'results'=>$perfumes
+            ]);
+        };
+        
     }
 
     public function show($id){
